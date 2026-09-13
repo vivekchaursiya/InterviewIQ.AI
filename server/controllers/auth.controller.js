9bce6e95c7b772380a878fe588fd24f828579e61
unknown <chaurasiyavivek29@gmail.com>
2026-09-13 06:07:21 -0700

    fixed auth

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -26,7 +26,11 @@ export const googleAuth=async(req,res)=>{
 }
 export const logOut=async(req,res)=>{
     try{
-        await res.clearCookie("token")
+        res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        });
         return res.status(200).json({message:"LogOut Successfully"})
 
     }
