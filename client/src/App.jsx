b6dc8728bc41e46e716e5a2e456697dfc6997ae3
unknown <chaurasiyavivek29@gmail.com>
2026-09-13 00:41:02 -0700

    Update production server URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,44 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Auth from "./pages/Auth"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
 import { useEffect } from "react";
 import axios from "axios";
 import { setUserData } from "./redux/userSlice";
-import {useDispatch} from 'react-redux'
+import { useDispatch } from "react-redux";
 import InterviewPage from "./pages/InterviewPage";
 import InterviewHistory from "./pages/InterviewHistory";
 import Pricing from "./pages/Pricing";
 import InterviewReport from "./pages/InterviewReport";
 
-
-export const serverURL="https://interviewiq-ai-1pyz.onrender.com";
-
-
-
+export const serverURL = "https://interviewiq-ai-1pyz.onrender.com";
 const App = () => {
-  const dispatch=useDispatch();
-  useEffect(()=>{
-    const getUser=async()=>{
-    try{
-      const result= await axios.get(serverURL + "/api/user/current-user",
-        {withCredentials:true})
-        dispatch(setUserData(result.data))
-    }catch(error){
-        console.log(error)
-        dispatch(setUserData(null))
-    }
-  }
-  getUser()
-  },[dispatch])
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const result = await axios.get(serverURL + "/api/user/current-user", {
+          withCredentials: true,
+        });
+        dispatch(setUserData(result.data));
+      } catch (error) {
+        console.log(error);
+        dispatch(setUserData(null));
+      }
+    };
+    getUser();
+  }, [dispatch]);
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/auth" element={<Auth/>}/>
-        <Route path="/interview" element={<InterviewPage/>}/>
-        <Route path="/history" element={<InterviewHistory/>}/>
-        <Route path="/pricing" element={<Pricing/>}/>
-        <Route path="/report/:id" element={<InterviewReport/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/interview" element={<InterviewPage />} />
+        <Route path="/history" element={<InterviewHistory />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/report/:id" element={<InterviewReport />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -8,21 +8,24 @@ import authRouter from "./routes/auth.route.js"
 import userRouter from "./routes/user.route.js"
 import interviewRouter from "./routes/interview.route.js"
 import paymentRouter from "./routes/payment.route.js"
-const app=express()
+const app = express()
 app.use(express.json())
 app.use(cookieParser())
-
+const allowedOrigins = [
+  "http://localhost:5173", // local
+  "https://interviewiq-ai-client-l7hv.onrender.com" // deployed frontend
+];
 app.use(cors({
-    origin: "https://interviewiq-ai-client-l7hv.onrender.com/",
-    credentials: true
+  origin: allowedOrigins,
+  credentials: true
 }))
 
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/interview",interviewRouter)
-app.use("/api/payment",paymentRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
+app.use("/api/interview", interviewRouter)
+app.use("/api/payment", paymentRouter)
 
-const PORT=process.env.PORT || 8000
+const PORT = process.env.PORT || 8000
 
 // connectDB()
 
